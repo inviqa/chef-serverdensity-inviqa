03e74743d66656c6e9e19ec69deac1e9de68be76
Shane Auckland <shane.auckland@gmail.com>
2015-03-17 00:29:55 +0000

    Prevent inappropriate apache url polling
    Having the Apache status URL makes the ServerDensity agent poll it, even
    when Apache is not installed. The URL should only be set if Apache
    monitoring is required.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
 default['serverdensity-inviqa']['tags'] = []
-
-default['serverdensity']['apache_status_url'] = 'http://127.0.0.1/server-status?auto'
--- recipes/apache.rb
+++ recipes/apache.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2015 Missguided, All Rights Reserved.
 
+node['serverdensity']['apache_status_url'] = 'http://127.0.0.1/server-status?auto'
+
 include_recipe 'apache2::default'
 
 apache_conf 'serverdensity' do
