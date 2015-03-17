17f30519ecb06e1e43f2a3323622fa1e8ddb4627
Shane Auckland <shane.auckland@gmail.com>
2015-03-17 08:41:43 +0000

    Bump patch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@inviqa.com'
 license          'Apache'
 description      'Provides additional ServerDensity configuration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'serverdensity', '~> 2.1.3'
 depends 'apache2'
